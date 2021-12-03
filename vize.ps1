c644a9c16549a3ff3252c7d1bd66772de7412c7a
emresahin2001 <67149817+emresahin2001@users.noreply.github.com>
2021-12-03 21:40:11 +0300

    Create vize.ps1

---
+++ vize.ps1
@@ -0,0 +1,29 @@
+﻿function arrayFilterSpace {
+param($array)
+$result = @()
+for($number = 0; $number -lt $array.length; $number++) {
+if ($array[$number]) {
+$result += $array[$number];
+}
+}
+return $result
+}
+
+$out = ""
+$number= 0
+foreach($item in Get-Process) {
+$find = netstat -ano | findstr $item.Id
+if ($find) {
+$result = arrayFilterSpace("$find".Split(' '));
+$serviceport= $result[1].Substring($result[1].lastIndexOf(':')+1)
+$servicename = $item.Name
+$serviceway = $item.Path
+$info = "Service: $servicename`r`n"
+$info += "Port: $serviceport`r`n"
+$out += $info
+$info
+$number++
+}
+}
+cd ~\Desktop
+$out | Out-File -FilePath./vizeodevi.txt
