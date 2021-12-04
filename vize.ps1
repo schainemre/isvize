e9c3e9bf7258f8b8053914fa400f847ad1bd8589
emresahin2001 <67149817+emresahin2001@users.noreply.github.com>
2021-12-04 18:20:35 +0300

    Update vize.ps1

--- vize.ps1
+++ vize.ps1
@@ -8,7 +8,6 @@ $result += $array[$number];
 }
 return $result
 }
-
 $out = ""
 $number= 0
 foreach($item in Get-Process) {
@@ -17,7 +16,6 @@ if ($find) {
 $result = arrayFilterSpace("$find".Split(' '));
 $serviceport= $result[1].Substring($result[1].lastIndexOf(':')+1)
 $servicename = $item.Name
-$serviceway = $item.Path
 $info = "Service: $servicename`r`n"
 $info += "Port: $serviceport`r`n"
 $out += $info
